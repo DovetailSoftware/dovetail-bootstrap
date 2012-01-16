93848b94914a9ef000ea56876f61cb4a28379975
kevm <scoundrel@gmail.com>
2012-01-16 16:12:57 -0600

    Random strings can be handy. *sigh*

--- source/Dovetail.SDK.Bootstrap/Extensions/StringExtensions.cs
+++ source/Dovetail.SDK.Bootstrap/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 using FubuCore;
 
@@ -22,5 +23,17 @@ namespace Dovetail.SDK.Bootstrap.Extensions
 			return result;
 		}
 
+        private static readonly Random _random = new Random(((int)DateTime.Now.Ticks));
+
+        public static string GetRandomString(int size)
+        {
+            var builder = new StringBuilder(size + 10);
+            for (var i = 0; i < size; i++)
+            {
+                var character = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * _random.NextDouble() + 65)));
+                builder.Append(character);
+            }
+            return builder.ToString().ToLower();
+        }
 	}
 }
