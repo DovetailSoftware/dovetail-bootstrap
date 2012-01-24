b8dbd8618b92c5a9ff942ef88a1cd0372973b2e3
kevm <scoundrel@gmail.com>
2012-01-24 12:36:51 -0600

    Web: moved database object count to About view

--- source/Web/Handlers/about/get_handler.cs
+++ source/Web/Handlers/about/get_handler.cs
@@ -1,12 +1,44 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using Dovetail.SDK.Bootstrap.Clarify;
 
 namespace Bootstrap.Web.Handlers.about
 {
     public class get_handler
     {
+        private readonly IClarifySession _userSession;
+
+        public get_handler(IClarifySession userSession)
+        {
+            _userSession = userSession;
+        }
+
         public AboutModel Execute(AboutRequest request)
         {
-            return new AboutModel{ Version = Assembly.GetCallingAssembly().GetName().Version.ToString()};
+            var sqlHelper = _userSession.CreateSqlHelper("SELECT count(*) from table_case");
+            var caseCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
+
+            sqlHelper.CommandText = "SELECT count(*) from table_subcase";
+            var subcaseCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
+
+            sqlHelper.CommandText = "SELECT count(*) from table_site";
+            var siteCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
+
+            sqlHelper.CommandText = "SELECT count(*) from table_contact";
+            var contactCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
+
+            sqlHelper.CommandText = "SELECT count(*) from table_probdesc";
+            var solutionCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
+
+            return new AboutModel
+                       {
+                           Version = Assembly.GetCallingAssembly().GetName().Version.ToString(),
+                           CaseCount = caseCount,
+                           ContactCount = contactCount,
+                           SiteCount = siteCount,
+                           SolutionCount = solutionCount,
+                           SubcaseCount = subcaseCount
+                       };
         }
     }
     
@@ -17,5 +49,11 @@ namespace Bootstrap.Web.Handlers.about
     public class AboutModel 
     {
         public string Version { get; set; }
+    
+        public int CaseCount { get; set; }
+        public int SubcaseCount { get; set; }
+        public int SiteCount { get; set; }
+        public int ContactCount { get; set; }
+        public int SolutionCount { get; set; }
     }
 }
--- source/Web/Handlers/home/get_handler.cs
+++ source/Web/Handlers/home/get_handler.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using Dovetail.SDK.Bootstrap.Clarify;
+﻿using System.Collections.Generic;
 using Dovetail.SDK.ModelMap;
 
 namespace Bootstrap.Web.Handlers.home
 {
     public class get_handler
     {
-        private readonly IClarifySession _userSession;
         private readonly IModelBuilder<UserOpenCaseListing> _listingBuilder;
 
-        public get_handler(IClarifySession userSession, IModelBuilder<UserOpenCaseListing> listingBuilder)
+        public get_handler(IModelBuilder<UserOpenCaseListing> listingBuilder)
         {
-            _userSession = userSession;
             _listingBuilder = listingBuilder;
         }
 
@@ -20,44 +16,14 @@ namespace Bootstrap.Web.Handlers.home
         {
             var userCases = _listingBuilder.GetAll();
 
-            var sqlHelper = _userSession.CreateSqlHelper("SELECT count(*) from table_case");
-            var caseCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
-            
-            sqlHelper.CommandText = "SELECT count(*) from table_subcase";
-            var subcaseCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
-
-            sqlHelper.CommandText = "SELECT count(*) from table_site";
-            var siteCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
-
-            sqlHelper.CommandText = "SELECT count(*) from table_contact";
-            var contactCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
-
-            sqlHelper.CommandText = "SELECT count(*) from table_probdesc";
-            var solutionCount = Convert.ToInt32(sqlHelper.ExecuteScalar());
-
-            return new HomeModel
-                       {
-                           Cases = userCases,
-                           CaseCount = caseCount,
-                           ContactCount = contactCount,
-                           SiteCount = siteCount,
-                           SolutionCount = solutionCount,
-                           SubcaseCount = subcaseCount
-                       };
+            return new HomeModel { Cases = userCases };
         }
     }
 
-    public class HomeRequest
-    {
-    }
+    public class HomeRequest { }
 
     public class HomeModel 
     {
         public IEnumerable<UserOpenCaseListing> Cases { get; set; }
-        public int CaseCount { get; set; }
-        public int SubcaseCount { get; set; }
-        public int SiteCount { get; set; }
-        public int ContactCount { get; set; }
-        public int SolutionCount { get; set; }
     }
 }
