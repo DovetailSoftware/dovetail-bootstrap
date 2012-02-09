027b29c8727f269e3d1afb1374e145fee202ad56
kevm <scoundrel@gmail.com>
2012-02-09 16:53:12 -0600

    Initial commit of swagger specification helper project.
    Swagger uses JSONSchema to represent the definitions of the models used by APIs. To get this to work I had to cajole JSON.Net with a special case serializer.

---
+++ source/Swagger.Net.Tests/swagger_resource_models_should_render_json_schema.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace Swagger.Net.Tests
+{
+    public class TestOutputModel 
+    {
+        public string id { get; set; }
+        public int count { get; set; }
+        public DateTime when { get; set; }
+    }
+
+    public class TestOutputModel2
+    {
+        public string id { get; set; }
+    }
+
+    public class TestOutputModel3
+    {
+        public string id { get; set; }
+        public TestOutputModel2[] Output2Models { get; set; }
+    }
+    
+    [TestFixture]
+    public class swagger_resource_models_should_render_json_schema
+    {
+        private Resource _resource;
+
+        [SetUp]
+        public void beforeEach()
+        {
+            _resource = new Resource
+                            {
+                                apiVersion = "1.0",
+                                basePath = "basePath",
+                                resourcePath = "resourcePath",
+                                swaggerVersion = "wwee!",
+                                apis = new[] { new API { path = "apiPath", description = "wee!", operations = new Operation[] { } } },
+                            };
+        }
+
+        [Test]
+        public void one_model()
+        {
+            var type = typeof(TestOutputModel);
+            _resource.models = new[] { type };
+            var modelJsonSchema = getJsonSchema(typeof(TestOutputModel));
+
+            var json = serializeToJson(_resource);
+            
+            StringAssert.Contains(modelJsonSchema, json);
+        }
+
+        [Test]
+        public void another_model()
+        {
+            var type = typeof(TestOutputModel3);
+            _resource.models = new[] {type};
+            var modelJsonSchema = getJsonSchema(type);
+
+            var json = serializeToJson(_resource);
+
+            StringAssert.Contains(modelJsonSchema, json);
+        }
+
+        [Test]
+        public void multiple_models()
+        {
+            var type1 = typeof(TestOutputModel);
+            var type2 = typeof(TestOutputModel2);
+            _resource.models = new[] { type1, type2 };
+            var type1JsonSchema = getJsonSchema(type1);
+            var type2JsonSchema = getJsonSchema(type2);
+
+            var json = serializeToJson(_resource);
+
+            StringAssert.Contains(type1JsonSchema, json);
+            StringAssert.Contains(type2JsonSchema, json);
+        }
+
+        private static string serializeToJson(Resource resource)
+        {
+            var json = JsonConvert.SerializeObject(resource, Formatting.None);
+            return json;
+        }
+
+
+        private static string getJsonSchema(Type type)
+        {
+            var sw = new StringWriter();
+            var jtw = new JsonTextWriter(sw) {Formatting = Formatting.None};
+            var jSchema = new JsonSchemaGenerator().Generate(type);
+            jSchema.Id = type.Name;
+
+            jSchema.WriteTo(jtw);
+
+            Trace.Write(sw);
+
+            return sw.ToString();
+        }
+    }
+}
---
+++ source/Swagger.Net/Models.cs
@@ -0,0 +1,92 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Swagger.Net
+{
+    public class Resources
+    {
+        public string basePath { get; set; }
+        public string swaggerVersion { get; set; }
+        public string apiVersion { get; set; }
+        public ResourceAPI[] apis { get; set; }
+    }
+
+    public class ResourceAPI
+    {
+        public string path { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Resource
+    {
+        public string basePath { get; set; }
+        public string resourcePath { get; set; }
+        public string swaggerVersion { get; set; }
+        public string apiVersion { get; set; }
+        public API[] apis { get; set; }
+        [JsonConverter(typeof(ToJsonSchemaConverter))]
+        public Type[] models { get; set; }
+    }
+
+    public class API
+    {
+        public string path { get; set; }
+        public string description { get; set; }
+        public Operation[] operations { get; set; }
+    }
+
+    public class Operation
+    {
+        public Parameter[] parameters { get; set; }
+        public ErrorResponses[] errorResponses { get; set; }
+        public string httpMethod { get; set; }
+        public string notes { get; set; }
+        public string responseTypeInternal { get; set; }
+        public string responseClass { get; set; }
+        public string nickname { get; set; }
+        public string parameterType { get; set; }
+
+    }
+
+    public class Parameter
+    {
+        public string path { get; set; }
+        public string defaultValue { get; set; }
+        public string description { get; set; }
+        public string dataType { get; set; }
+        public AllowableValues[] allowableValues { get; set; }
+        public string allowMultiple { get; set; }
+        public string parameterType { get; set; }
+    }
+
+    public class AllowableValues
+    {
+        public string max { get; set; }
+        public string min { get; set; }
+        public string[] values { get; set; }
+        public string valueType { get; set; }
+    }
+
+    public class ErrorResponses
+    {
+        public string reason { get; set; }
+        public int code { get; set; }
+    }
+
+    public class Model
+    {
+        public string id { get; set; }
+        public ModelProperty[] properties { get; set; }
+    }
+
+    public class ModelProperty
+    {
+        public string path { get; set; }
+        public string defaultValue { get; set; }
+        public string description { get; set; }
+        public string dataType { get; set; }
+        public AllowableValues[] allowableValues { get; set; }
+        public string allowMultiple { get; set; }
+        public string parameterType { get; set; }
+    }
+}
