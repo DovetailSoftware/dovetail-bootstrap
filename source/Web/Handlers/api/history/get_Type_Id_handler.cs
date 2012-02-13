557a5c0408b3c770b6d9948b8fecb24928d1afb5
kevm <scoundrel@gmail.com>
2012-02-13 15:13:09 -0600

    History limiter now takes a number of days rather than a datetime

--- source/Web/Handlers/api/history/get_Type_Id_handler.cs
+++ source/Web/Handlers/api/history/get_Type_Id_handler.cs
@@ -1,25 +1,30 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Dovetail.SDK.Bootstrap;
 using Dovetail.SDK.Bootstrap.History;
+using Dovetail.SDK.ModelMap;
 
 namespace Bootstrap.Web.Handlers.api.history
 {
 	public class get_Type_Id_handler
 	{
         private readonly IHistoryAssembler _historyAssembler;
+	    private readonly ISystemTime _systemTime;
 
-        public get_Type_Id_handler(IHistoryAssembler historyAssembler) 
+	    public get_Type_Id_handler(IHistoryAssembler historyAssembler, ISystemTime systemTime)
         {
             _historyAssembler = historyAssembler;
+            _systemTime = systemTime;
         }
 
-        public HistoryViewModel Execute(HistoryRequest request)
+	    public HistoryViewModel Execute(HistoryRequest request)
         {
 			var workflowObject = WorkflowObject.Create(request.Type, request.Id); 
 			
-			if (request.Since.HasValue)
-            {
-            	return _historyAssembler.GetHistorySince(workflowObject, request.Since.Value);
+			if (request.DaysOfHistory> 0)
+			{
+			    var since = _systemTime.Now.Subtract(TimeSpan.FromDays(request.DaysOfHistory));
+            	return _historyAssembler.GetHistorySince(workflowObject, since);
             }
 
         	return _historyAssembler.GetHistory(workflowObject);
@@ -28,9 +33,11 @@ namespace Bootstrap.Web.Handlers.api.history
 
 	public class HistoryRequest : IApi 
     {
+        [Required]
         public string Type { get; set; }
+        [Required]
 		public string Id { get; set; }
-		public DateTime? Since { get; set; }
-		public string Timezone { get; set; }
+
+		public int DaysOfHistory { get; set; }
     }
 }
