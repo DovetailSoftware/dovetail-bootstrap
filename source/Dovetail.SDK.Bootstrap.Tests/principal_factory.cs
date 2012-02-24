1dd78d101c4b3c2e12b7c7c325a6458c8323c15c
kevm <scoundrel@gmail.com>
2012-02-24 09:40:48 -0600

    Updated nuget dependencies

--- source/Dovetail.SDK.Bootstrap.Tests/principal_factory.cs
+++ source/Dovetail.SDK.Bootstrap.Tests/principal_factory.cs
@@ -6,10 +6,43 @@ using Rhino.Mocks;
 
 namespace Dovetail.SDK.Bootstrap.Tests
 {
-    public class principal_factory
+    public class when_creating_principal
     {
         [TestFixture]
-        public class when_creating_principal : Context<PrincipalFactory>
+        public class  happy_path : Context<PrincipalFactory>
+        {
+            private IIdentity _identity;
+            private IPrincipal _result;
+
+            public override void Given()
+            {
+                _identity = MockFor<IIdentity>();
+                _identity.Stub(s => s.Name).Return("annie");
+
+                var clarifySession = MockFor<IClarifySession>();
+                clarifySession.Stub(s => s.Permissions).Return(new[] { "role1", "role2" });
+
+                MockFor<IClarifySessionCache>().Expect(s => s.GetSession(_identity.Name)).Return(clarifySession);
+                _result = _cut.CreatePrincipal(_identity);
+            }
+
+            [Test]
+            public void should_get_identified_user_session()
+            {
+                MockFor<IClarifySessionCache>().VerifyAllExpectations();
+            }
+
+            [Test]
+            public void should_return_principal_with_session_permissions_as_roles()
+            {
+                _result.IsInRole("role1").ShouldBeTrue();
+                _result.IsInRole("role2").ShouldBeTrue();
+                _result.IsInRole("notinrole").ShouldBeFalse();
+            }
+        }
+
+        [TestFixture]
+        public class  sad_path : Context<PrincipalFactory>
         {
             private IIdentity _identity;
             private IPrincipal _result;
