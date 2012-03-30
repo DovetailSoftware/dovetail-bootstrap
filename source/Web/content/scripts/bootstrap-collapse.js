c45660a2463eeb51753f0172b10ddbbf9fe6b254
kevm <scoundrel@gmail.com>
2012-03-30 16:40:30 -0500

    Twitter Bootstrap updated to 2.0.2

--- source/Web/content/scripts/bootstrap-collapse.js
+++ source/Web/content/scripts/bootstrap-collapse.js
@@ -1,45 +1,45 @@
 ﻿/* =============================================================
-* bootstrap-collapse.js v2.0.0
-* http://twitter.github.com/bootstrap/javascript.html#collapse
-* =============================================================
-* Copyright 2012 Twitter, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* ============================================================ */
+ * bootstrap-collapse.js v2.0.2
+ * http://twitter.github.com/bootstrap/javascript.html#collapse
+ * =============================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
 
-!function ($) {
+!function( $ ){
 
-    "use strict"
+  "use strict"
 
-    var Collapse = function (element, options) {
-        this.$element = $(element)
-        this.options = $.extend({}, $.fn.collapse.defaults, options)
+  var Collapse = function ( element, options ) {
+  	this.$element = $(element)
+    this.options = $.extend({}, $.fn.collapse.defaults, options)
 
-        if (this.options["parent"]) {
-            this.$parent = $(this.options["parent"])
-        }
-
-        this.options.toggle && this.toggle()
+    if (this.options["parent"]) {
+      this.$parent = $(this.options["parent"])
     }
 
-    Collapse.prototype = {
+    this.options.toggle && this.toggle()
+  }
 
-        constructor: Collapse
+  Collapse.prototype = {
+
+    constructor: Collapse
 
   , dimension: function () {
       var hasWidth = this.$element.hasClass('width')
       return hasWidth ? 'width' : 'height'
-  }
+    }
 
   , show: function () {
       var dimension = this.dimension()
@@ -48,25 +48,25 @@
         , hasData
 
       if (actives && actives.length) {
-          hasData = actives.data('collapse')
-          actives.collapse('hide')
-          hasData || actives.data('collapse', null)
+        hasData = actives.data('collapse')
+        actives.collapse('hide')
+        hasData || actives.data('collapse', null)
       }
 
       this.$element[dimension](0)
       this.transition('addClass', 'show', 'shown')
       this.$element[dimension](this.$element[0][scroll])
 
-  }
+    }
 
   , hide: function () {
       var dimension = this.dimension()
       this.reset(this.$element[dimension]())
       this.transition('removeClass', 'hide', 'hidden')
       this.$element[dimension](0)
-  }
+    }
 
-  , reset: function (size) {
+  , reset: function ( size ) {
       var dimension = this.dimension()
 
       this.$element
@@ -74,15 +74,17 @@
         [dimension](size || 'auto')
         [0].offsetWidth
 
-      this.$element.addClass('collapse')
-  }
+      this.$element[size ? 'addClass' : 'removeClass']('collapse')
 
-  , transition: function (method, startEvent, completeEvent) {
+      return this
+    }
+
+  , transition: function ( method, startEvent, completeEvent ) {
       var that = this
         , complete = function () {
             if (startEvent == 'show') that.reset()
             that.$element.trigger(completeEvent)
-        }
+          }
 
       this.$element
         .trigger(startEvent)
@@ -91,46 +93,46 @@
       $.support.transition && this.$element.hasClass('collapse') ?
         this.$element.one($.support.transition.end, complete) :
         complete()
-  }
+  	}
 
   , toggle: function () {
       this[this.$element.hasClass('in') ? 'hide' : 'show']()
+  	}
+
   }
 
-    }
+  /* COLLAPSIBLE PLUGIN DEFINITION
+  * ============================== */
 
-    /* COLLAPSIBLE PLUGIN DEFINITION
-    * ============================== */
-
-    $.fn.collapse = function (option) {
-        return this.each(function () {
-            var $this = $(this)
+  $.fn.collapse = function ( option ) {
+    return this.each(function () {
+      var $this = $(this)
         , data = $this.data('collapse')
         , options = typeof option == 'object' && option
-            if (!data) $this.data('collapse', (data = new Collapse(this, options)))
-            if (typeof option == 'string') data[option]()
-        })
-    }
+      if (!data) $this.data('collapse', (data = new Collapse(this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
 
-    $.fn.collapse.defaults = {
-        toggle: true
-    }
+  $.fn.collapse.defaults = {
+    toggle: true
+  }
 
-    $.fn.collapse.Constructor = Collapse
+  $.fn.collapse.Constructor = Collapse
 
 
-    /* COLLAPSIBLE DATA-API
-    * ==================== */
+ /* COLLAPSIBLE DATA-API
+  * ==================== */
 
-    $(function () {
-        $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function (e) {
-            var $this = $(this), href
+  $(function () {
+    $('body').on('click.collapse.data-api', '[data-toggle=collapse]', function ( e ) {
+      var $this = $(this), href
         , target = $this.attr('data-target')
           || e.preventDefault()
           || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
         , option = $(target).data('collapse') ? 'toggle' : $this.data()
-            $(target).collapse(option)
-        })
+      $(target).collapse(option)
     })
+  })
 
-} (window.jQuery)
+}( window.jQuery );
