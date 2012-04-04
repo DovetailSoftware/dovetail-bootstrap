60805e746c3678e4e9a1550501faab370e00cbbf
kevm <scoundrel@gmail.com>
2012-04-04 15:17:43 -0500

    Htmlization now wraps lines in <p> tags

--- source/Dovetail.SDK.Bootstrap/Extensions/StringExtensions.cs
+++ source/Dovetail.SDK.Bootstrap/Extensions/StringExtensions.cs
@@ -7,18 +7,17 @@ namespace Dovetail.SDK.Bootstrap.Extensions
 {
 	public static class StringExtensions
 	{
-		private static readonly Regex find2OrMoreLineBreaks = new Regex(@"(\s*)(\r\n|(\n|\r)){2,}", RegexOptions.Multiline);
-		private static readonly Regex findEndOfLine = new Regex(@"\r\n|(\n|\r)", RegexOptions.Multiline);
-		private static readonly Regex urlFinderRegEx = new Regex(@"(?<link>(?<protocol>ftp|http|https|mailto|file|webcal):(?:(?:[A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(?:#(?:[a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?(?:[A-Za-z0-9$_+!*();/?:~-]))");
-		public static string ToHtml(this String toHtml)
+        private static readonly Regex findLineBreaks = new Regex(@"\s*(\n\r|\n)\s*");
+        private static readonly Regex findLine = new Regex(@"^(.*)$", RegexOptions.Multiline);
+        private static readonly Regex urlFinderRegEx = new Regex(@"(?<link>(?<protocol>ftp|http|https|mailto|file|webcal):(?:(?:[A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(?:#(?:[a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?(?:[A-Za-z0-9$_+!*();/?:~-]))");
+        public static string ToHtml(this String toHtml)
 		{
 			if (toHtml.IsEmpty()) return String.Empty;
 
-			var result = find2OrMoreLineBreaks.Replace(toHtml, Environment.NewLine);
+			var result = findLineBreaks.Replace(toHtml, "\n");
 
-			result = findEndOfLine.Replace(result, "<br/>");
-
-			result = urlFinderRegEx.Replace(result, @"<a href=""${link}"">${link}</a>");
+            result = findLine.Replace(result, "<p>$1</p>");
+            result = urlFinderRegEx.Replace(result, @"<a href=""${link}"">${link}</a>");
 
 			return result;
 		}
