4533501050079d042143805866db13100db8d217
Kevin Miller <scoundrel@gmail.com>
2012-10-09 15:53:20 -0500

    Added support for searchable fields in filtered fields
    * revved sdk to get new SchemaCache hooks needed

--- source/Dovetail.SDK.ModelMap/NextGen/FilterConfig.cs
+++ source/Dovetail.SDK.ModelMap/NextGen/FilterConfig.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Globalization;
 using FChoice.Common.Data;
+using FChoice.Foundation.Clarify;
+using FChoice.Foundation.Clarify.Schema;
 using FubuCore;
 
 namespace Dovetail.SDK.ModelMap.NextGen
@@ -13,15 +16,26 @@ namespace Dovetail.SDK.ModelMap.NextGen
 	{
 		public override string Render(WhereItem config, SqlHelper db)
 		{
+			var isOracle = DbProviderFactory.IsOracle();
+
 			int paramNum = db.Parameters.Count;
 			var paramName = "param" + paramNum.ToString(CultureInfo.InvariantCulture);
 
 			var databaseParameterFactory = new DatabaseParameterFactory(DbProviderFactory.Provider);
 
-			var parameter = databaseParameterFactory.Create(paramName, config.Field, config.Value);
-			db.Parameters.Add(paramName, parameter);
+			var fieldName = config.Field.Name;
+			var value = config.Value;
 
-			return "{0}.{1} {2} {{{3}}}".ToFormat(config.Alias, config.Field.Name, Operator, paramNum);
+			if(isOracle && config.Field.IsSearchable && config.Field.CommonType == (int) SchemaCommonType.String)
+			{
+				fieldName = "S_" + fieldName;
+				value = Convert.ToString(value).ToUpperInvariant();
+			}
+
+			var parameter = databaseParameterFactory.Create(paramName, config.Field, value);
+			db.Parameters.Add(paramName, parameter);
+			
+			return "{0}.{1} {2} {{{3}}}".ToFormat(config.Alias, fieldName, Operator, paramNum);
 		}
 
 		protected abstract string Operator { get; }
