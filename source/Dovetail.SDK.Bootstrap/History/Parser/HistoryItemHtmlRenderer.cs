1932b9651aa159d07c8a54780765ee42c2b7717f
Kevin Miller <scoundrel@gmail.com>
2013-10-11 12:28:02 -0500

    History html renderer was clearning content before original messages
    #DovetailSoftware/agent/846

--- source/Dovetail.SDK.Bootstrap/History/Parser/HistoryItemHtmlRenderer.cs
+++ source/Dovetail.SDK.Bootstrap/History/Parser/HistoryItemHtmlRenderer.cs
@@ -5,7 +5,6 @@ using FubuCore;
 
 namespace Dovetail.SDK.Bootstrap.History.Parser
 {
-	//TODO pull our renderers into a their own types
 	public interface IHistoryItemHtmlRenderer
 	{
 		string Render(IEnumerable<IItem> items);
@@ -13,98 +12,89 @@ namespace Dovetail.SDK.Bootstrap.History.Parser
 
 	public class HistoryItemHtmlRenderer : IHistoryItemHtmlRenderer
 	{
-        private StringBuilder _output;
+		private static long _idIndex;
 
-        private static long _idIndex;
+		public string Render(IEnumerable<IItem> items)
+		{
+			var output = new StringBuilder();
 
-        public string Render(IEnumerable<IItem> items)
-        {
-			_output = new StringBuilder();
+			foreach (var item in items)
+			{
+				if (item.GetType().CanBeCastTo<EmailHeader>())
+				{
+					renderEmailHeader(item as EmailHeader, output);
+					continue;
+				}
 
-            foreach (var item in items)
-            {
-                if(item.GetType().CanBeCastTo<EmailHeader>())
-                {
-                    renderEmailHeader(item as EmailHeader);
-                    continue;
-                }
+				if (item.GetType().CanBeCastTo<BlockQuote>())
+				{
+					renderBlockQuote(item as BlockQuote, output);
+					continue;
+				}
 
-                if (item.GetType().CanBeCastTo<BlockQuote>())
-                {
-                    renderBlockQuote(item as BlockQuote);
-                    continue;
-                }
+				if (item.GetType().CanBeCastTo<OriginalMessage>())
+				{
+					renderOriginalMessage(item as OriginalMessage, output);
+					continue;
+				}
 
-                if (item.GetType().CanBeCastTo<OriginalMessage>())
-                {
-                    renderOriginalMessage(item as OriginalMessage);
-                    continue;
-                }
+				renderItem(item, output);
+			}
 
-                renderItem(item);
-            }
+			return output.ToString();
+		}
 
-            return _output.ToString();
-        }
+		private static void renderEmailHeader(EmailHeader emailHeader, StringBuilder output)
+		{
+			_idIndex += 1;
+			var id = "emailHeader" + _idIndex;
+			output.AppendLine(@"<div id=""{0}"" class=""history-email-header"">".ToFormat(id));
 
-        private void renderEmailHeader(EmailHeader emailHeader)
-        {
-            _idIndex += 1;
-            var id = "emailHeader" + _idIndex;
-			var shouldCollapse = emailHeader.Headers.Count() > 1;
-            _output
-				//.AppendLine(@"<div id=""{0}"" class=""history-email-header collapse in""><i class=""icon-envelope"" title=""Click to expand"" data-toggle=""collapse"" data-target=""#{0}""></i>"
-				.AppendLine(@"<div id=""{0}"" class=""history-email-header {1}""><i class=""icon-envelope"" {2} data-target=""#{0}""></i>"
-				.ToFormat(id, shouldCollapse ? "collapse in" : "", shouldCollapse ? @"data-toggle=""collapse"" title=""Click to expand""" : ""));
+			var emailHeaderItem = emailHeader.Headers.First();
+			output.AppendLine(@"<h5 class=""history-inline-header"">{0} : {1}</h5>".ToFormat(emailHeaderItem.Title.ToLower().Capitalize(), emailHeaderItem.Text));
 
-            var emailHeaderItem = emailHeader.Headers.First();
-            _output
-				.AppendLine(@"<h5 class=""history-inline-header"">{0} : {1}</h5>"
-				.ToFormat(emailHeaderItem.Title.ToLower().Capitalize(), emailHeaderItem.Text));
-
-        	var rest = emailHeader.Headers.Skip(1).ToArray();
+			var rest = emailHeader.Headers.Skip(1).ToArray();
 			if (rest.Any())
 			{
-				_output.AppendLine(@"<div class=""history-inline-content""><ul>");
+				output.AppendLine(@"<div class=""history-inline-content""><ul>");
 				foreach (var header in rest)
 				{
-					_output.AppendLine(@"<li><span class=""email-header-name"">{0}</span> <span class=""email-header-text"">{1}</span></li>".ToFormat(header.Title.ToLower().Capitalize(), header.Text));
+					output.AppendLine(@"<li><span class=""email-header-name"">{0}</span> <span class=""email-header-text"">{1}</span></li>".ToFormat(header.Title.ToLower().Capitalize(), header.Text));
 				}
-				_output.Append(@"</ul></div>");
+				output.Append(@"</ul></div>");
 			}
-        	
-        	_output.AppendLine("</div>");
-        }
 
-        private void renderBlockQuote(BlockQuote blockQuote)
-        {
-            _output.AppendLine(@"<blockquote>");
+			output.AppendLine("</div>");
+		}
 
-            foreach (var line in blockQuote.Lines)
-            {
-                _output.AppendLine(line + "<br/>");
-            }
+		private static void renderBlockQuote(BlockQuote blockQuote, StringBuilder output)
+		{
+			output.AppendLine(@"<blockquote>");
 
-            _output.AppendLine(@"</blockquote>");
-        }
+			foreach (var line in blockQuote.Lines)
+			{
+				output.AppendLine(line + "<br/>");
+			}
 
-        private void renderOriginalMessage(OriginalMessage message)
-        {
-            _idIndex += 1;
-            var id = "originalMessage" + _idIndex;
-            _output.AppendLine(@"<div id=""{0}"" class=""history-inline-message collapse in ""><i class=""icon-comment"" title=""Click to expand"" data-toggle=""collapse"" data-target=""#{0}""></i>".ToFormat(id));
+			output.AppendLine(@"</blockquote>");
+		}
 
-            _output.AppendLine(@"<h5 class=""history-inline-header"">{0}</h5><div class=""history-inline-content"">".ToFormat(message.Header));
+		private void renderOriginalMessage(OriginalMessage message, StringBuilder output)
+		{
+			_idIndex += 1;
+			var id = "originalMessage" + _idIndex;
+			output.AppendLine(@"<div id=""{0}"" class=""history-inline-message""></i>".ToFormat(id));
 
-            Render(message.Items);
+			output.AppendLine(@"<h5 class=""history-inline-header"">{0}</h5><div class=""history-inline-content"">".ToFormat(message.Header));
 
-            _output.AppendLine(@"</div></div>");
-        }
+			output.Append(Render(message.Items));
 
+			output.AppendLine(@"</div></div>");
+		}
 
-        private void  renderItem(IItem item)
-        {
-            _output.AppendLine("<p>{0}</p>".ToFormat(item));
-        }
-    }
+		private static void renderItem(IItem item, StringBuilder output)
+		{
+			output.AppendLine("<p>{0}</p>".ToFormat(item));
+		}
+	}
 }
