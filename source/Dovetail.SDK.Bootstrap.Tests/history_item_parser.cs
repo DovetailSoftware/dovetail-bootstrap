0accddb238309057000b9433cc3ee70c46cb118f
Kevin Miller <scoundrel@gmail.com>
2013-10-17 13:57:10 -0500

    History Parser degrades more gracefully
    * Fake results returned when parse error happens.
    * Error is logged with details suitable to reproduce the error.

--- source/Dovetail.SDK.Bootstrap.Tests/history_item_parser.cs
+++ source/Dovetail.SDK.Bootstrap.Tests/history_item_parser.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using Dovetail.SDK.Bootstrap.History.Configuration;
 using Dovetail.SDK.Bootstrap.History.Parser;
 using NUnit.Framework;
 using FubuCore;
+using Rhino.Mocks;
 
 namespace Dovetail.SDK.Bootstrap.Tests
 {
@@ -10,11 +14,13 @@ namespace Dovetail.SDK.Bootstrap.Tests
 	public class history_item_parser
 	{
 		private HistoryItemParser _itemParser;
+		private ILogger _logger;
 
 		[SetUp]
 		public void beforeEach()
 		{
-			_itemParser = new HistoryItemParser(new HistoryParsers(new HistorySettings()));
+			_logger = MockRepository.GenerateStub<ILogger>();
+			_itemParser = new HistoryItemParser(new HistoryParsers(new HistorySettings()), _logger);
 		}
 
 		[Test]
@@ -57,6 +63,52 @@ namespace Dovetail.SDK.Bootstrap.Tests
 			var original = (OriginalMessage)items[1];
 			original.Header.ShouldContain("Kevin Miller wrote");
 			original.Items.Count().ShouldEqual(2);
+		}
+
+		[Test]
+		public void email_that_cannot_be_parsed_returns_fake_emaillog()
+		{
+			const string input = "__BEGIN EMAIL_HEADER\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_FROM\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_DATE\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_TO\r\n__END EMAIL_HEADER\r\n\n\nThanks, Hank\r\n";
+			_logger.Expect(e => e.LogError(Arg<string>.Matches(s => s.Contains(input)), Arg<Exception>.Is.Anything));
+
+			var emailLog = _itemParser.ParseEmailLog(input);
+
+			emailLog.Header.Headers.Count().ShouldEqual(0);
+			emailLog.Items.Count().ShouldEqual(1);
+			var item = emailLog.Items.First();
+			((Content) item).Text.ShouldEqual(input);
+		}
+
+		[Test]
+		public void email_that_cannot_be_parsed_logs_error()
+		{
+			const string input = "__BEGIN EMAIL_HEADER\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_FROM\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_DATE\r\npl-PL_HistoryBuilderTokens:LOG_EMAIL_TO\r\n__END EMAIL_HEADER\r\n\n\nThanks, Hank\r\n";
+
+			_logger.Expect(e => e.LogError(Arg<string>.Matches(s => s.Contains(input)), Arg<Exception>.Is.Anything));
+
+			_itemParser.ParseEmailLog(input);
+
+			_logger.VerifyAllExpectations();
+		}
+	}
+
+	public static class CultureExtension
+	{
+		public static void As(this CultureInfo asCulture, Action action)
+		{
+			var previousCulture = Thread.CurrentThread.CurrentCulture;
+			Thread.CurrentThread.CurrentCulture = asCulture;
+			Thread.CurrentThread.CurrentUICulture = asCulture;
+
+			try
+			{
+				action();
+			}
+			finally
+			{
+				Thread.CurrentThread.CurrentCulture = previousCulture;
+				Thread.CurrentThread.CurrentUICulture = previousCulture;
+			}
 		}
 	}
 }
--- source/Dovetail.SDK.Bootstrap/History/Parser/HistoryItemParser.cs
+++ source/Dovetail.SDK.Bootstrap/History/Parser/HistoryItemParser.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using FubuCore;
 using Sprache;
 
 namespace Dovetail.SDK.Bootstrap.History.Parser
@@ -12,20 +14,52 @@ namespace Dovetail.SDK.Bootstrap.History.Parser
 	public class HistoryItemParser : IHistoryItemParser
 	{
 		private readonly HistoryParsers _historyParser;
+		private readonly ILogger _logger;
 
-		public HistoryItemParser(HistoryParsers historyParser)
+		public HistoryItemParser(HistoryParsers historyParser, ILogger logger)
 		{
 			_historyParser = historyParser;
+			_logger = logger;
 		}
-		
+
 		public IEnumerable<Content> ParseContent(string input)
 		{
-			return _historyParser.Content.Many().End().Parse(input);
+			try
+			{
+				return _historyParser.Content.Many().End().Parse(input);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError("Could not parse content. Contents:\n\n{0}".ToFormat(input), e);
+				return fakeContent(input);
+			}
 		}
 
 		public EmailLog ParseEmailLog(string input)
 		{
-			return _historyParser.LogEmail.Parse(input);
+			try
+			{
+				return _historyParser.LogEmail.Parse(input);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError("Could not parse email log. Contents:\n\n{0}".ToFormat(input), e);
+				return fakeEmailLog(input);
+			}
+		}
+
+		private static IEnumerable<Content> fakeContent(string input)
+		{
+			return new[] {new Content {Text = input}};
+		}
+
+		private static EmailLog fakeEmailLog(string input)
+		{
+			return new EmailLog
+			{
+				Header = new EmailHeader { Headers = new EmailHeaderItem[0] },
+				Items = new[] {new Content {Text = input}}
+			};
 		}
 	}
 }
