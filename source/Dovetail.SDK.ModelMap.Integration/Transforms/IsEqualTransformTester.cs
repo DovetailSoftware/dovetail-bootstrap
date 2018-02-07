ddf9fc1dc6fab7308d4b66c5f84965d578c06eec
Mpawelczyk <mpawelczyk@dovetailsoftware.com>
2018-02-07 17:53:40 -0600

    Added test for equality of int and bool, both ways.

--- source/Dovetail.SDK.ModelMap.Integration/Transforms/IsEqualTransformTester.cs
+++ source/Dovetail.SDK.ModelMap.Integration/Transforms/IsEqualTransformTester.cs
@@ -9,7 +9,7 @@ namespace Dovetail.SDK.ModelMap.Integration.Transforms
 	public class IsEqualTransformTester
 	{
 		[Test]
-		public void evaluates_equality()
+		public void evaluates_equality_of_int_and_string()
 		{
 			var services = new InMemoryServiceLocator();
 			services.Add<IMappingVariableExpander>(new MappingVariableExpander(new MappingVariableRegistry(new List<IMappingVariableSource>()), services));
@@ -20,14 +20,65 @@ namespace Dovetail.SDK.ModelMap.Integration.Transforms
 			});
 
 			var data = new ModelData();
-			data["status"] = 0;
 
 			var context = new TransformContext(data, arguments, new InMemoryServiceLocator());
 			var transform = new IsEqualTransform();
 
+			data["status"] = 0;
 			transform.Execute(context).ShouldEqual(true);
 
 			data["status"] = 1;
+			transform.Execute(context).ShouldEqual(false);
+
+			arguments = new TransformArguments(services, new Dictionary<string, object>
+			{
+				{ "field", "status" },
+				{ "value", 0 }
+			});
+
+			context = new TransformContext(data, arguments, new InMemoryServiceLocator());
+
+			data["status"] = "0";
+			transform.Execute(context).ShouldEqual(true);
+
+			data["status"] = "1";
+			transform.Execute(context).ShouldEqual(false);
+		}
+
+		[Test]
+		public void evaluates_equality_of_int_and_bool()
+		{
+			var services = new InMemoryServiceLocator();
+			services.Add<IMappingVariableExpander>(new MappingVariableExpander(new MappingVariableRegistry(new List<IMappingVariableSource>()), services));
+			var arguments = new TransformArguments(services, new Dictionary<string, object>
+			{
+				{ "field", "status" },
+				{ "value", true }
+			});
+
+			var data = new ModelData();
+
+			var context = new TransformContext(data, arguments, new InMemoryServiceLocator());
+			var transform = new IsEqualTransform();
+
+			data["status"] = 1;
+			transform.Execute(context).ShouldEqual(true);
+
+			data["status"] = 0;
+			transform.Execute(context).ShouldEqual(false);
+
+			arguments = new TransformArguments(services, new Dictionary<string, object>
+			{
+				{ "field", "status" },
+				{ "value", 1 }
+			});
+
+			context = new TransformContext(data, arguments, new InMemoryServiceLocator());
+
+			data["status"] = true;
+			transform.Execute(context).ShouldEqual(true);
+
+			data["status"] = false;
 			transform.Execute(context).ShouldEqual(false);
 		}
 	}
