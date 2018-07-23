3c6f4aa48535566b5aa7e8faba534cbb8d716220
jmarnold <rncodepoet@gmail.com>
2018-07-23 09:41:13 -0500

    Add unit test for nested partials

---
+++ source/Dovetail.SDK.History.Tests/Serialization/nested_partials.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Dovetail.SDK.History.Tests.Serialization
+{
+	[TestFixture]
+	public class nested_partials
+	{
+		private HistoryMapParsingScenario theScenario;
+
+		[SetUp]
+		public void SetUp()
+		{
+			theScenario = HistoryMapParsingScenario.Create(_ =>
+			{
+				_.UseFile("nested-partial.history.config");
+				_.UseFile("partials1.partial.config");
+				_.UseFile("partials2.partial.config");
+				_.UseFile("partials3.partial.config");
+				_.UseFile("partials4.partial.config");
+			});
+		}
+
+		[Test]
+		public void verify_instructions()
+		{
+			theScenario.WhatDoIHave();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			theScenario.CleanUp();
+		}
+	}
+}
