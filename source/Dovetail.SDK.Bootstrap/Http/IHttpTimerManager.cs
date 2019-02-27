fb67d05c14311fd1e30e349fdc7fec9b427736b2
jmarnold <rncodepoet@gmail.com>
2019-02-27 15:52:52 -0600

    Wrap timer execution in a try/catch

--- source/Dovetail.SDK.Bootstrap/Http/IHttpTimerManager.cs
+++ source/Dovetail.SDK.Bootstrap/Http/IHttpTimerManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using FubuCore;
 
 namespace Dovetail.SDK.Bootstrap.Http
@@ -13,11 +14,13 @@ namespace Dovetail.SDK.Bootstrap.Http
 	{
 		private readonly IServiceLocator _services;
 		private readonly IHttpApplicationStorage _storage;
+		private readonly ILogger _logger;
 
-		public HttpTimerManager(IServiceLocator services, IHttpApplicationStorage storage)
+		public HttpTimerManager(IServiceLocator services, IHttpApplicationStorage storage, ILogger logger)
 		{
 			_services = services;
 			_storage = storage;
+			_logger = logger;
 		}
 
 		public void Start<TTService>() where TTService : IHttpIntervalService
@@ -31,9 +34,17 @@ namespace Dovetail.SDK.Bootstrap.Http
 			var service = _services.GetInstance<TTService>();
 			var timer = new Timer(state =>
 			{
-				_services
-					.GetInstance<TTService>()
-					.Execute();
+				try
+				{
+					_services
+						.GetInstance<TTService>()
+						.Execute();
+				}
+				catch (Exception e)
+				{
+					_logger.LogError("Error executing " + typeof(TTService).Name, e);
+				}
+
 			});
 
 			timer.Change(service.Interval, service.Interval);
